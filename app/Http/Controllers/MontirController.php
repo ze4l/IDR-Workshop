9f1f738051bf872ab42313fa4b67e82977893510
Dikypratama08 <201220027@unmuhpnk.ac.id>
2023-07-24 12:34:28 +0700

    membuat tabel montir dan kehadiran

---
+++ app/Http/Controllers/MontirController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Montir;
+use App\Http\Requests\StoreMontirRequest;
+use App\Http\Requests\UpdateMontirRequest;
+
+class MontirController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMontirRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Montir $montir)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Montir $montir)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMontirRequest $request, Montir $montir)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Montir $montir)
+    {
+        //
+    }
+}
