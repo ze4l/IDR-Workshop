e95caf982f61648fa9d071a66b64a6ed61075302
Dikypratama08 <201220027@unmuhpnk.ac.id>
2023-07-26 03:28:57 +0700

    menambahkan halaman montir

--- database/seeders/MontirSeeder.php
+++ database/seeders/MontirSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MontirSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class MontirSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('montirs')->insert([
+            'namamontir'=>'Agung',
+            'usiamontir'=>'45',
+            'divisi'=>'Mekanik AC'
+            
+        ]);
     }
 }
